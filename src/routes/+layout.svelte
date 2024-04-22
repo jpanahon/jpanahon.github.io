a81a6a6fa6694988d18a1bd41a9c1691953bcf5e
jpanahon <36034467+jpanahon@users.noreply.github.com>
2024-04-22 13:12:47 -0400

    trying again 2 attack of the clones

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,6 @@
 <script>
     import "../app.css";
+    import { base } from "$app/paths";
     let year = new Date().getFullYear();
 
     let showNavBar = false;
@@ -8,18 +9,18 @@
 {#if showNavBar}
     <nav class="bg-[#a85fff] p-4 text-3xl fixed h-screen w-fit border-black border-r-2">
         <ul class="mt-16">
-            <li class="mb-8"><a href="/">Home</a></li>
-            <li class="mb-8"><a href="/projects">Projects</a></li>
-            <li class="mb-8"><a href="/jovic-panahon-resume.pdf" target="_blank">Resume</a></li>
-            <li class="mb-8"><a href="/about">About</a></li>
-            <li class="mb-8"><a href="/contact">Contact</a></li>
+            <li class="mb-8"><a href="{base}/">Home</a></li>
+            <li class="mb-8"><a href="{base}/projects">Projects</a></li>
+            <li class="mb-8"><a href="{base}/jovic-panahon-resume.pdf" target="_blank">Resume</a></li>
+            <li class="mb-8"><a href="{base}/about">About</a></li>
+            <li class="mb-8"><a href="{base}/contact">Contact</a></li>
         </ul>
     </nav>
 {/if}
 
 <header class="flex items-center justify p-4 border-[#a85fff] border-b-2 sticky">
     <button class="text-6xl text-left fixed" on:click={() => (showNavBar = !showNavBar)}> &equiv; </button>
-    <h1 class="m-auto text-6xl font-bold"><a href="/">Jovic's Space</a></h1>
+    <h1 class="m-auto text-6xl font-bold"><a href="{base}/">Jovic's Space</a></h1>
 </header>
 
 <slot />
